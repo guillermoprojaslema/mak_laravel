26e07c7ac8b91c9af5f2bf47debac20a0a5511f6
Guillermo Rojas <guillermorojaslema@gmail.com>
2019-03-19 18:18:16 -0300

    Footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,36 @@
+<footer class="bg-dark" style="color: rgba(255, 255, 255, 0.5)">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-lg-4">
+                    <br>
+                    <img class="logo-footer" src="{{asset('images/logos/logo_200x100.png')}}" alt="logo-footer"
+                         data-at2x="assets/img/logo.png">
+                    <p>
+                        Lorem ipsum dolor sit amet
+                    </p>
+                    <p><a href="#">Our Team</a></p>
+                </div>
+                <div class="col-md-4 col-lg-4 offset-lg-1">
+                    <br>
+                    <h3>Contact</h3>
+                    <p><i class="fa fa-map-marker-alt"></i> Via Rossini 10, 10136 Turin Italy</p>
+                    <p><i class="fa fa-phone"></i> Phone: (0039) 333 12 68 347</p>
+                    <p><i class="fa fa-envelope"></i> Email: <a href="mailto:hello@domain.com">hello@domain.com</a></p>
+                    <p><i class="fa fa-skype"></i> Skype: you_online</p>
+                </div>
+                <div class="col-md-4 col-lg-3">
+                    <br>
+                    <h3>Follow us</h3>
+                    <p>
+                        <a href="#"><i class="fa fa-facebook"></i></a>
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="#"><i class="fa fa-google-plus-g"></i></a>
+                        <a href="#"><i class="fa fa-instagram"></i></a>
+                        <a href="#"><i class="fa fa-pinterest"></i></a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
