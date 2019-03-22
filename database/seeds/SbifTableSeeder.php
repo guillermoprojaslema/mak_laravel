f02e62e8d4e37dfda4ba701735c89ac4d0d54be8
Guillermo Rojas <guillermorojaslema@gmail.com>
2019-03-22 17:15:55 -0300

    Los indicadores ecónimicos, son usados para búsqueda.

--- database/seeds/SbifTableSeeder.php
+++ database/seeds/SbifTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Sbif;
+use GuzzleHttp\Client;
 
 class SbifTableSeeder extends Seeder
 {
@@ -13,13 +14,62 @@ class SbifTableSeeder extends Seeder
     public function run()
     {
 
+        $base_uri_dolar = 'https://api.sbif.cl/api-sbifv3/recursos_api/dolar?apikey=' . env('SBIF_API_KEY') . '&formato=json';
+        $base_uri_euro = 'https://api.sbif.cl/api-sbifv3/recursos_api/euro?apikey=' . env('SBIF_API_KEY') . '&formato=json';
+        $base_uri_uf = 'https://api.sbif.cl/api-sbifv3/recursos_api/uf?apikey=' . env('SBIF_API_KEY') . '&formato=json';
+        $base_uri_utm = 'https://api.sbif.cl/api-sbifv3/recursos_api/utm?apikey=' . env('SBIF_API_KEY') . '&formato=json';
+        $base_uri_ipc = 'https://api.sbif.cl/api-sbifv3/recursos_api/ipc?apikey=' . env('SBIF_API_KEY') . '&formato=json';
+
         $sbif = new Sbif();
-        $sbif->id = 0;
-        $sbif->dolar = 0;
-        $sbif->euro = 0;
-        $sbif->uf = 0;
-        $sbif->ipc = 0;
-        $sbif->utm = 0;
+        /*Dólar*/
+        $client = new Client([
+            'base_uri' => $base_uri_dolar,
+            'timeout' => 10.0
+        ]);
+        $response = $client->request('GET', '');
+        $res = $response->getBody()->getContents();
+        $sbif->dolar = (float)str_replace(',', '.', str_replace('.', '', json_decode($res)->Dolares[0]->Valor));
+
+
+        /*Euro*/
+        $client = new Client([
+            'base_uri' => $base_uri_euro,
+            'timeout' => 10.0
+        ]);
+        $response = $client->request('GET', '');
+        $res = $response->getBody()->getContents();
+        $sbif->euro = (float)str_replace(',', '.', str_replace('.', '', json_decode($res)->Euros[0]->Valor));
+
+
+        /*UF*/
+        $client = new Client([
+            'base_uri' => $base_uri_uf,
+            'timeout' => 10.0
+        ]);
+        $response = $client->request('GET', '');
+        $res = $response->getBody()->getContents();
+        $sbif->uf = (float)str_replace(',', '.', str_replace('.', '', json_decode($res)->UFs[0]->Valor));
+
+
+        /*UF*/
+        $client = new Client([
+            'base_uri' => $base_uri_ipc,
+            'timeout' => 10.0
+        ]);
+        $response = $client->request('GET', '');
+        $res = $response->getBody()->getContents();
+        $sbif->ipc = (float)str_replace(',', '.', str_replace('.', '', json_decode($res)->IPCs[0]->Valor));
+
+
+        /*UF*/
+        $client = new Client([
+            'base_uri' => $base_uri_utm,
+            'timeout' => 10.0
+        ]);
+
+        $response = $client->request('GET', '');
+        $res = $response->getBody()->getContents();
+        $sbif->utm = (int)str_replace(',', '.', str_replace('.', '', json_decode($res)->UTMs[0]->Valor));
         $sbif->save();
 
     }
--- resources/views/propiedades/index.blade.php
+++ resources/views/propiedades/index.blade.php
@@ -22,7 +22,16 @@
                             {{$oferta->bano}} <i class="fas fa-bath"></i>
                             <b>Sup: </b>{{$oferta->metros_cuadrados}} m<sup>2</sup>
                             <b>Cons: </b>{{$oferta->metros_cuadrados_construidos}} m<sup>2</sup>
-                            <i class="fa fa-dollar"></i> {{number_format($oferta->precio, 0, ',', '.')}}
+                            <div class="precio_clp">{{'$ '.number_format($oferta->precio  , 0, ',', '.')}} CLP</div>
+                            <div class="precio_uf">
+                                {{'$ '. number_format((float)$oferta->precio / $sbif->uf, 2, ',', '.')}} UF
+                            </div>
+                            <div class="precio_usd">
+                                {{'$ '. number_format((float)$oferta->precio / $sbif->dolar, 2, ',', '.')}} USD
+                            </div>
+                            <div class="precio_eur">
+                                {{'€ '. number_format((float)$oferta->precio / $sbif->euro,  2, ',', '.')}} EUR
+                            </div>
                         </div>
                     </div>
                 @empty
@@ -73,16 +82,47 @@
                             <div class="label-rating"><b>Sup: </b>{{$destacado->metros_cuadrados}} m<sup>2</sup></div>
                             <div class="label-rating"><b>Cons: </b>{{$destacado->metros_cuadrados_construidos}}
                                 m<sup>2</sup></div>
-                            <div class="label-rating"><i
-                                        class="fa fa-dollar"></i> {{number_format($oferta->precio, 0, ',', '.')}}</div>
+                            <div class="label-rating precio_clp">{{'$ '.number_format($oferta->precio  , 0, ',', '.')}}
+                                CLP
+                            </div>
+                            <div class="label-rating precio_uf">
+                                {{'$ '. number_format((float)$oferta->precio / $sbif->uf, 2, ',', '.')}} UF
+                            </div>
+                            <div class="label-rating precio_usd">
+                                {{'$ '. number_format((float)$oferta->precio / $sbif->dolar, 2, ',', '.')}} USD
+                            </div>
+                            <div class="label-rating precio_eur">
+                                {{'€ '. number_format((float)$oferta->precio / $sbif->euro,  2, ',', '.')}} EUR
+                            </div>
                         </div> <!-- rating-wrap.// -->
                     </figcaption>
                     <div class="bottom-wrap">
                         <a href="{{route('propiedades.show', $destacado->id)}}"
                            class="btn btn-sm btn-danger float-right">Detalles</a>
                         <div class="price-wrap h5">
-                            <span class="price-new">${{$destacado->precio}}</span>
-                            <del class="price-old">${{$destacado->precio}}</del>
+                            <span class="price-new precio_clp">{{'$ '.number_format($oferta->precio  , 0, ',', '.')}}
+                                CLP</span>
+                            <del class="price-old precio_clp">{{'$ '.number_format($oferta->precio  , 0, ',', '.')}}
+                                CLP
+                            </del>
+
+                            <span class="price-new precio_uf">{{'$ '. number_format((float)$oferta->precio / $sbif->uf, 2, ',', '.')}}
+                                UF</span>
+                            <del class="price-old precio_uf">{{'$ '. number_format((float)$oferta->precio / $sbif->uf, 2, ',', '.')}}
+                                UF
+                            </del>
+
+                            <span class="price-new precio_usd">{{'$ '. number_format((float)$oferta->precio / $sbif->dolar, 2, ',', '.')}}
+                                USD</span>
+                            <del class="price-old precio_usd">{{'$ '. number_format((float)$oferta->precio / $sbif->dolar, 2, ',', '.')}}
+                                USD
+                            </del>
+
+                            <span class="price-new precio_eur">{{'€ '. number_format((float)$oferta->precio / $sbif->euro,  2, ',', '.')}}
+                                EUR</span>
+                            <del class="price-old precio_eur">{{'€ '. number_format((float)$oferta->precio / $sbif->euro,  2, ',', '.')}}
+                                EUR
+                            </del>
                         </div> <!-- price-wrap.// -->
                     </div> <!-- bottom-wrap.// -->
                 </figure>
@@ -100,6 +140,13 @@
 
     <script>
         $(document).ready(function () {
+
+            $('.precio_clp').show();
+            $('.precio_usd').hide();
+            $('.precio_eur').hide();
+            $('.precio_uf').hide();
+
+
             $(".owl-carousel").owlCarousel({
                 items: 1,
                 loop: true,
@@ -123,8 +170,34 @@
                 showLabels: true,
                 isRange: true
             });
+
+            $('#divisa').change(function () {
+                var divisa = $("#divisa option:selected").val();
+                if (divisa == 'CLP') {
+                    $('.precio_clp').show();
+                    $('.precio_usd').hide();
+                    $('.precio_eur').hide();
+                    $('.precio_uf').hide();
+                } else if (divisa == 'USD') {
+                    $('.precio_usd').show();
+                    $('.precio_clp').hide();
+                    $('.precio_eur').hide();
+                    $('.precio_uf').hide();
+
+                } else if (divisa == 'EUR') {
+                    $('.precio_eur').show();
+                    $('.precio_usd').hide();
+                    $('.precio_uf').hide();
+                    $('.precio_clp').hide();
+
+                } else if (divisa == 'UF') {
+                    $('.precio_uf').show();
+                    $('.precio_clp').hide();
+                    $('.precio_usd').hide();
+                    $('.precio_eur').hide();
+
+                }
+            });
         });
-
-
     </script>
 @endpush
