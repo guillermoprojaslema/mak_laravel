34a58adc95f7b3de9eeaae8fd3d7ed0f0a806667
Guillermo Rojas <guillermorojaslema@gmail.com>
2019-04-15 21:30:54 -0400

    Seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,19 +14,17 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(RegionTableSeeder::class);
         $this->call(ComunaTableSeeder::class);
-        if (env('APP_ENV') != 'production') {
-            $this->call(ClienteTableSeeder::class);
-            $this->call(BarrioTableSeeder::class);
-            $this->call(EdificioTableSeeder::class);
-            $this->call(ConserjeTableSeeder::class);
-            $this->call(CasaTableSeeder::class);
-            $this->call(ApartamentoTableSeeder::class);
-            $this->call(LocalComercialTableSeeder::class);
-            $this->call(OficinaTableSeeder::class);
-            $this->call(EstacionamientoTableSeeder::class);
-            $this->call(BodegaTableSeeder::class);
-            $this->call(TerrenoTableSeeder::class);
-        }
+        $this->call(ClienteTableSeeder::class);
+        $this->call(BarrioTableSeeder::class);
+        $this->call(EdificioTableSeeder::class);
+        $this->call(ConserjeTableSeeder::class);
+        $this->call(CasaTableSeeder::class);
+        $this->call(ApartamentoTableSeeder::class);
+        $this->call(LocalComercialTableSeeder::class);
+        $this->call(OficinaTableSeeder::class);
+        $this->call(EstacionamientoTableSeeder::class);
+        $this->call(BodegaTableSeeder::class);
+        $this->call(TerrenoTableSeeder::class);
         $this->call(PaginasTableSeeder::class);
         $this->call(EmpleadosTableSeeder::class);
         $this->call(SbifTableSeeder::class);
